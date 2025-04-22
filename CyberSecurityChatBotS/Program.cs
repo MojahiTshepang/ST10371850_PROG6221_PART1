2ed3733de1d4e527b25dca53c2b578596fc84d64
Tshepang Mojahi <130612261+MojahiTshepang@users.noreply.github.com>
2025-04-22 16:16:15 +0200

    Initial commit

---
+++ CyberSecurityChatBotS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CyberSecurityChatBotS
+{
+
+    using System.Media;
+
+    static void PlayVoiceGreeting()
+    {
+        SoundPlayer player = new SoundPlayer("welcome.wav");
+        player.PlaySync();  // Plays the file before continuing
+    }
+}
